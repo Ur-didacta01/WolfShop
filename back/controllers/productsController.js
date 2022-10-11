98c70c65e726f78c71f0c9df2235423cd9cbc7ba
Ur-didacta01 <111081864+Ur-didacta01@users.noreply.github.com>
2022-10-11 16:46:45 -0500

    Cambio en el proyecto
    Cambio agregando más conexiones a base de datos y agregando categorías de productos de la tienda.

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,6 +1,17 @@
+const producto=require("../models/productos")
 exports.getProducts=(req,res,next) =>{
     res.status(200).json({
         sucess:true,
-        message: "En esta ruta ud podrá ver todos los videojuegos"
+        message: "En esta ruta ud podrá ver todos los productos"
+    })
+}
+
+
+exports.newProduct=async(req,res,next)=>{
+    const product= await producto.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
     })
 }
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,8 +1,9 @@
 const express=require("express")
 const router=express.Router();
 
-const {getProducts} = require("../controllers/productsController")
+const {getProducts, newProduct} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
 
-router.route('/productos').get(getProducts) //Establecemos desde que ruta queremos ver el getVideogames
+router.route('/productos').get(getProducts)  //Establecemos desde que ruta queremos ver el getProducts
+router.route('/producto/nuevo').post(newProduct);
 
 module.exports=router;
