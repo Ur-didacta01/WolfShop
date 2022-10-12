321fe49743010d40327dcefd7a0dcc4de9ae5e7f
Ur-didacta01 <111081864+Ur-didacta01@users.noreply.github.com>
2022-10-12 09:12:48 -0500

    Cambio en el proyecto
    modificación del proyecto añadiendo fetch, modificación y eliminación.

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,17 +1,95 @@
 const producto=require("../models/productos")
-exports.getProducts=(req,res,next) =>{
+const fetch =(url)=>import('node-fetch').then(({default:fetch})=>fetch(url));
+exports.getProducts=async(req,res,next) =>{
+    const productos= await producto.find();
     res.status(200).json({
         sucess:true,
-        message: "En esta ruta ud podrá ver todos los productos"
+        count: productos.length,
+        productos
+    })
+}
+
+
+//Ver un producto por ID
+exports.getProductById= async(req,res,next)=>{
+    const product= await producto.findById(req.params.id)
+    if (!product){
+        return res.status(404).json({
+            success:false,
+            message: "No encontramos el producto"
+        })
+    }
+    res.status(200).json({
+        success:true,
+        mensaje:"Aquí debajo encuentras información sobre el producto:",
+        product
+    })
+}
+
+//Update un producto
+exports.updateProduct= async (req,res,next)=>{
+    let product= await producto.findById(req.params.id)
+    if (!product){ //Verifico que el producto no exista
+        return res.status(404).json({
+            sucess:false,
+            message: "No encontramos el producto"
+        })
+    }
+//Si existe el producto, ejecuto la actualización
+    product= await producto.findByIdAndUpdate(req.params.id, req.body,{
+        nnew:true,
+        runValidators:true
+    });
+    //Respondo ok si el producto se actualizó
+    res.status(200).json({
+        success:true,
+        message:"Tu producto está actualizado",
+        producto
+    })
+}
+
+//Eliminar el producto
+exports.deleteProduct=async (req,res,next)=>{
+    const product= await producto.findById(req.params.id)
+    if (!product){ //Verifico que el producto no exista
+        return res.status(404).json({
+            sucess:false,
+            message: "No encontramos el producto"
+        })
+    }
+
+    await product.remove();
+    res.status(200).json({
+        success:true,
+        mensaje:"Producto eliminado"
     })
 }
 
 
 exports.newProduct=async(req,res,next)=>{
     const product= await producto.create(req.body);
-
     res.status(201).json({
         success:true,
         product
     })
 }
+
+//FETCH
+
+function verProductos(){
+    fetch('http://localhost:4000/api/productos')
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
+
+//verProductos(); Llamamos al metodo para probar la consulta
+
+function verProductoPorID(id){
+    fetch('http://localhost:4000/api/producto/'+id)
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
+
+//verProductoPorID('6345db1d6165e0dd8a117d71'); probamos el método con id
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,9 +1,12 @@
 const express=require("express")
 const router=express.Router();
 
-const {getProducts, newProduct} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
+const {getProducts, newProduct, getProductById, updateProduct, deleteProduct} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
 
 router.route('/productos').get(getProducts)  //Establecemos desde que ruta queremos ver el getProducts
-router.route('/producto/nuevo').post(newProduct);
+router.route('/producto/nuevo').post(newProduct); //Estableciendo ruta
+router.route('/producto/:id').get(getProductById); //Ruta para consultar id
+router.route('/producto/:id').put(updateProduct); //Creación ruta de actualización
+router.route('/producto/:id').delete(deleteProduct);// Creación ruta eliminación
 
 module.exports=router;
