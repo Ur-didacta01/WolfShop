ddf00fec8775f00fcaa2b428a6cf4929c46f5290
Ur-didacta01 <111081864+Ur-didacta01@users.noreply.github.com>
2022-10-13 17:02:11 -0500

    Cambios en el front
    Cambios en el front, react y adición del logo wolfshop

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda de productos WolfShop</h1>
+      <img src="./images/wolfshop.png" alt="Aquí a el logo"></img>
+    </div>
+  );
+}
+
+export default App;
