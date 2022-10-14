80ed178a0488df9fc1cafc434059d311bcb8c08f
Ur-didacta01 <111081864+Ur-didacta01@users.noreply.github.com>
2022-10-14 07:47:05 -0500

    Cambio página web proyecto
    Adición de botones como buscar, iniciar sesión, entre otros.

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import React from 'react';
+import Header from './components/layout/Header';
+import { Footer } from './components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Tienda de productos WolfShop</h1>
-      <img src="./images/wolfshop.png" alt="Aquí a el logo"></img>
+      <Header />
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <center>Contenido aqui</center>
+      <Footer />
     </div>
   );
 }
---
+++ front/src/components/layout/Footer.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+
+export const Footer = () => {
+  return (
+    <Fragment>
+        <footer className="py-1">
+            <p className="text-center mt-1">
+                WolfShop store -2022- derechos reservados
+            </p>
+        </footer>
+
+    </Fragment>
+  )
+}
