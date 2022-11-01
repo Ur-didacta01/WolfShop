062147e7500f550e852c0c80457f4ba2078e3c67
Ur-didacta01 <111081864+Ur-didacta01@users.noreply.github.com>
2022-11-01 17:49:26 -0500

    token

--- back/controllers/authController.js
+++ back/controllers/authController.js
@@ -1,6 +1,8 @@
 const User = require("../models/auth")
 const ErrorHandler=require("../utils/errorHandler")
-const catchAsyncErrors=require("../middleware/catchAsyncErrors")
+const catchAsyncErrors=require("../middleware/catchAsyncErrors");
+const { TokenExpiredError } = require("jsonwebtoken");
+const tokenEnviado = require("../utils/jwtToken");
 
 //Registrar un nuevo usuario /api//usuario/registrar
 
@@ -16,10 +18,32 @@ exports.registroUsuario=catchAsyncErrors(async(req, res, next)=>{
             url: "https://png.pngtree.com/png-vector/20191110/ourlarge/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg"
         }
     })
-
-    res.status(201).json({
-        success:true,
-        user
+    const token = user.getJwtToken();
+    tokenEnviado(user, 201,res)
     })
 
+
+//Iniciar sesion
+exports.loginUser = catchAsyncErrors(async(req, res, next)=>{
+    const {email, password} = req.body;
+
+    //revisar si los campos estan completos
+    if (!email || !password){
+        return next(new ErrorHandler("Por favor ingrese email & contraseña, no te cuesta nada :)", 400))
+    }
+
+    //Buscar al usuario en la base de datos
+    const user = await User.findOne({email}).select("+password")
+
+    if(!user){
+        return next(new ErrorHandler("Email o contraseña inválidos", 401))
+    }
+
+    //Chequear la contraseña
+    const contraseñaOK= await user.compararPass(password);
+
+    if(!contraseñaOK){
+        return next(new ErrorHandler("Contraseña inválida", 401))
+    }
+    tokenEnviado(user, 200,res)
 })
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,9 +1,10 @@
 const express=require("express")
 const router=express.Router();
 
-const {getProducts, newProduct, getProductById, updateProduct, deleteProduct} = require("../controllers/productsController") //Traemos la respuesta json desde el controlador
+const {getProducts, newProduct, getProductById, updateProduct, deleteProduct} = require("../controllers/productsController"); //Traemos la respuesta json desde el controlador
+const { isAuthenticatedUser,authorizeRoles } = require("../middleware/auth");
 
-router.route('/productos').get(getProducts)  //Establecemos desde que ruta queremos ver el getProducts
+router.route('/productos').get(isAuthenticatedUser, authorizeRoles("admin","user"), getProducts)  //Establecemos desde que ruta queremos ver el getProducts
 router.route('/producto/nuevo').post(newProduct); //Estableciendo ruta
 router.route('/producto/:id').get(getProductById); //Ruta para consultar id
 router.route('/producto/:id').put(updateProduct); //Creación ruta de actualización
